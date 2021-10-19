be1ab6747e1844a04d3d430b913ac406d9d37528
Camel <hackball08@gmail.com>
2021-10-20 00:04:45 +0700

    add stock hist rest api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  const TradingView = window.TradingView;
+  new TradingView.widget(
+    {
+    "width": 980,
+    "height": 610,
+    "symbol": "SET:SET",
+    "interval": "D",
+    "timezone": "Asia/Bangkok",
+    "theme": "light",
+    "style": "1",
+    "locale": "in",
+    "toolbar_bg": "#f1f3f6",
+    "enable_publishing": false,
+    "allow_symbol_change": true,
+    "container_id": "tradingview_97d1f"
+    }
+    );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="tradingview-widget-container">
+        <div id="tradingview_97d1f"></div>
+        <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/symbols/SET-SET/" rel="noopener" target="_blank"><span class="blue-text">SET Chart</span></a> by TradingView</div>
+      </div>
     </div>
   );
 }
