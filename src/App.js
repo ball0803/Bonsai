3aa9b8431939503cc59433ea26c282bcaab7cd35
Keetawin <khitawanlimxrun@gmail.com>
2021-10-20 21:24:30 +0700

    Template

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import './App.css';
-import { useEffect } from 'react';
+// routes
+import Router from './routes';
+// theme
+import ThemeConfig from './theme';
+import GlobalStyles from './theme/globalStyles';
+// components
+import ScrollToTop from './components/ScrollToTop';
+import { BaseOptionChartStyle } from './components/charts/BaseOptionChart';
 
-function App() {
+// ----------------------------------------------------------------------
 
-  const TradingView = window.TradingView;
-  new TradingView.widget(
-    {
-    "width": 980,
-    "height": 610,
-    "symbol": "SET:SET",
-    "interval": "D",
-    "timezone": "Asia/Bangkok",
-    "theme": "light",
-    "style": "1",
-    "locale": "in",
-    "toolbar_bg": "#f1f3f6",
-    "enable_publishing": false,
-    "allow_symbol_change": true,
-    "container_id": "tradingview_97d1f"
-    }
-    );
+export default function App() {
   return (
-    <div className="App">
-      <div class="tradingview-widget-container">
-        <div id="tradingview_97d1f"></div>
-        <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/symbols/SET-SET/" rel="noopener" target="_blank"><span class="blue-text">SET Chart</span></a> by TradingView</div>
-      </div>
-    </div>
+    <ThemeConfig>
+      <ScrollToTop />
+      <GlobalStyles />
+      <BaseOptionChartStyle />
+      <Router />
+    </ThemeConfig>
   );
 }
-
-export default App;
---
+++ src/_mocks_/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Keetawan Limaroon',
+  email: 'khitawanlimxrun@gmail.com',
+  photoURL: '/static/mock-images/avatars/avatar_default.jpg'
+};
+
+export default account;
